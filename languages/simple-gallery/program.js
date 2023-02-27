3d0be54a2f69e316b23ece281eefea6bc0669783
Humberto <10425094+berto@users.noreply.github.com>
2023-02-27 01:45:36 +0000

    create program
    program that reads config and outputs a gallery of images

---
+++ languages/simple-gallery/program.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
+// read the config file
+const config = fs.readFileSync('config.txt', 'utf-8').split('\n');
+
+// extract the path and image extensions
+const dirPath = config[0];
+const extensions = config[1].split(',');
+
+// extract the columns to display
+const columns = config[2].split(',');
+
+// create an empty array to store image metadata
+const images = [];
+
+// read the directory and filter files by extension
+fs.readdirSync(dirPath).forEach(file => {
+  const ext = path.extname(file).substring(1);
+  if (extensions.includes(ext)) {
+    // read the file properties
+    const stats = fs.statSync(`${dirPath}/${file}`);
+
+    // add properties to array
+    const image = {};
+    image['src'] = `${dirPath}/${file}`;
+    image['name_full'] = file;
+    image['name'] = path.parse(file).name;
+    image['extension'] = ext;
+    image['size'] = stats.size;
+    image['date_created'] = stats.birthtime;
+    image['date_updated'] = stats.mtime;
+    images.push(image);
+  }
+});
+
+// create the HTML table
+let html = '<table>';
+html += '<tr>';
+columns.forEach(col => {
+  html += `<th>${col}</th>`;
+});
+html += '</tr>';
+images.forEach(image => {
+  html += '<tr>';
+  columns.forEach(col => {
+    if (col === 'Image') {
+      html += `<td><img src="${image['src']}" width="200"></td>`;
+    } else {
+      html += `<td>${image[col.toLowerCase()]}</td>`;
+    }
+  });
+  html += '</tr>';
+});
+html += '</table>';
+
+// write the HTML to a file
+fs.writeFileSync('gallery.html', html);
